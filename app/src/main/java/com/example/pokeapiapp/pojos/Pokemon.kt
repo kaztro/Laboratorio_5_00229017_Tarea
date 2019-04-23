9ee6e65398c1f1d570373aba7ec2f73b49cd11cb
kaztro <00229017@uca.edu.sv>
2019-04-23 16:04:55 -0600

    The pojo has been finished

--- app/src/main/java/com/example/pokeapiapp/pojos/Pokemon.kt
+++ app/src/main/java/com/example/pokeapiapp/pojos/Pokemon.kt
@@ -1,5 +1,45 @@
 package com.example.pokeapiapp.pojos
 
-data class Pokemon {
+import android.os.Parcel
+import android.os.Parcelable
 
+data class Pokemon (
+    val name: String = "N/A",
+    val fsttype: String = "N/A",
+    val sndtype: String = "N/A",
+    val weight: String = "N/A",
+    val height: String = "N/A",
+    val url:String = "N/A",
+    val sprite:String = "N/A"
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        name = parcel.readString(),
+        fsttype = parcel.readString(),
+        sndtype = parcel.readString(),
+        weight = parcel.readString(),
+        height = parcel.readString(),
+        url = parcel.readString(),
+        sprite = parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(fsttype)
+        parcel.writeString(sndtype)
+        parcel.writeString(weight)
+        parcel.writeString(height)
+        parcel.writeString(url)
+        parcel.writeString(sprite)
+
+    }
+
+    override fun describeContents() = 0
+
+    companion object {
+        @JvmField val CREATOR = object : Parcelable.Creator<Pokemon> {
+            override fun createFromParcel(parcel: Parcel): Pokemon = Pokemon(parcel)
+            override fun newArray(size: Int): Array<Pokemon?> = arrayOfNulls(size)
+        }
+    }
 }
