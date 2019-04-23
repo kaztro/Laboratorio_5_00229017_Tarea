2674671a5135fda0347a29fb35e5fa9c51adbeca
kaztro <00229017@uca.edu.sv>
2019-04-23 15:23:06 -0600

    I'm on it right now

--- app/src/main/java/com/example/pokeapiapp/network/NetworkUtils.kt
+++ app/src/main/java/com/example/pokeapiapp/network/NetworkUtils.kt
@@ -1,7 +1,36 @@
 package com.example.pokeapiapp.network
 
+import android.net.Uri
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
 class NetworkUtils {
 
-    val POKEMONS_API_BASEURL = "https://pokeapi.co/api/v2/\n"
-    val TOKEN_API = ""
+    val POKEMONS_API_BASEURL = "https://pokeapi.co/api/v2/"
+
+    fun buildtSearchUrl(pokeName: String) : URL {
+        val builtUri = Uri.parse(POKEMONS_API_BASEURL)
+            .buildUpon()
+            .appendQueryParameter("t",pokeName)
+            .build()
+        return try{ URL(builtUri.toString()) }
+               catch (e : MalformedURLException) { URL("") }
+    }
+
+    @Throws(IOException::class)
+    fun getResponseFromHttpUrl(url: URL) : String{
+        val urlConnection = url.openConnection() as HttpURLConnection
+        try {
+            val `in` = urlConnection.inputStream
+
+            val scanner = Scanner(`in`)
+            scanner.useDelimiter("\\A")
+
+            val hasInput = scanner.hasNext()
+            return if(hasInput) { scanner.next() } else { "" }
+        } finally { urlConnection.disconnect() }
+    }
 }
